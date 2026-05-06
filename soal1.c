4828bce02bae56191fb8d523228727ccacd65754
MNT-EL08 <salmansantika123@gmail.com>
2026-05-06 10:47:33 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -1,5 +1,65 @@
+/** EL2008 Praktikum Pemecahan Masalah dengan Pemrograman 2025/2026
+ *   Modul               : 4
+ *   Hari dan Tanggal    : Selasa, 6 Mei 2026
+ *   Nama (NIM)          : Salman Santika Abdullah (13224084)
+ *   Nama File           : soal2.c
+ *   Deskripsi           : Membuat sistem yang dapat menghitung jumlah durasi antrian
+ */
+
+// C Program to demonstrate how to Implement a queue
+#include <stdbool.h>
 #include <stdio.h>
 
-int main(void) {
-    return 0;
+// 1. Mendefinisikan struktur Node 
+struct Pasien {
+    char kode[3];
+    int id;
+    int durasi;
+    struct Pasien* next;
+};
+
+// Pointer global 
+struct Pasien* head = NULL;
+
+// Fungsi untuk menambahkan Pasien baru 
+void tambahPasien(char* kode, int id, int durasi) {
+    struct Pasien* nodeBaru = (struct Pasien*)malloc(sizeof(struct Pasien));
+    strcpy(nodeBaru->kode, kode);
+    nodeBaru->id = id;
+    nodeBaru->durasi = durasi;
+    nodeBaru->next = NULL;
+
+    // Cek linked list masih kosong
+    if (head == NULL) {
+        head = nodeBaru;
+    } else {
+        struct Pasien* temp = head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = nodeBaru;
+    }
 }
+
+
+
+int main (){
+    int N;
+    char inputKode[3];
+    int inputId;
+    int inputDurasi;
+    scanf("%d", &N);
+
+    for (int i = 0; i < 0; i++){
+        tambahPasien(inputKode, inputId, inputDurasi);
+    }
+    //operasi menghitung durasi
+
+
+    
+
+    
+}
+
+// referensi
+// https://www.geeksforgeeks.org/c/queue-in-c/
